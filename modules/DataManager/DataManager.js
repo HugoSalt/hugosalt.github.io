4bb7c1fa54ef38f6d5c2bc6e0234398aba56f37f
Akynna <tim.nguyen@epfl.ch>
2017-11-27 18:31:28 +0100

    implement common filter function for Genre, Platform and Publisher

--- modules/DataManager/DataManager.js
+++ modules/DataManager/DataManager.js
@@ -13,32 +13,54 @@ export default class DataManager {
 
   /*
   * The functions below are used to filter our data given a particular feature.
-  * TODO : implement filter methods and update
+  * TODO : implement update methods
   */
 
   //
   setGenre(genres) {
     this.genres = genres;
-    this.filteredData = this.data.filter();
+    this.filteredData = this.data.filter(genres, "Genre");
     this.updateComponents();
   }
 
   setPlatform(platforms) {
     this.platforms = platforms;
-    this.filteredData = this.data.filter();
+    this.filteredData = this.data.filter(platforms, "Platform");
     this.updateComponents();
   }
 
   setTimeInterval(timeInterval) {
     this.timeInterval = timeInterval;
-    this.filteredData = this.data.filter();
+    this.filteredData = this.data.filter(); // TODO: special filter for Years ?
     this.updateComponents();
   }
 
   setPublisher(publishers) {
     this.publishers = publishers;
-    this.filteredData = this.data.filter();
+    this.filteredData = this.data.filter(publishers, "Publisher");
     this.updateComponents();
+  }
+
+  /*
+  *  Returns a filtered data given a list of selected elements and the
+  *  feature evaluated.
+  *
+  *  => Eg. selectedElements = [EA, Ubisoft, Nintendo, ...]
+  *         feature = "Publisher"
+  */
+  filter(selectedElements, feature) {
+    let filteredData = this.data;
+
+    // Keep only games that have as feature the selected elements
+    for(selectedElement in selectedElements) {
+      filteredData = filteredData.reduce(
+        (filteredData, game) => {
+          if(game[feature] = selectedElement) { filteredData.push(game); }
+          return filteredData;
+        }, filteredData);
+    }
+    
+    return filteredData;
   }
 
   updateComponents() {
@@ -46,4 +68,6 @@ export default class DataManager {
       component.update(this.filteredData);
     }
   }
+
+
 }
