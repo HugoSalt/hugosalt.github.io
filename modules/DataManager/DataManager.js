f040b2933eda4441b2fe03920600d066cc4fbf4f
Mike Bardet <mikebardet@tsf-484-wpa-0-136.epfl.ch>
2017-11-28 14:14:14 +0100

    Implement DataManager and part of GenreBar

--- modules/DataManager/DataManager.js
+++ modules/DataManager/DataManager.js
@@ -1,14 +1,14 @@
 export default class DataManager {
 
-  constructor(container_id, data, components_to_update) {
+  constructor(data, components_to_update, genres, platforms, publishers, timeInterval) {
     this.data = data;
     this.filteredData = data;
     this.components_to_update = components_to_update;
-    this.genres = "any";
-    this.platforms = "any";
-    this.publishers = "any";
-    this.timeInterval = [1980, 2015];
-    this.components_to_update();
+    this.genres = genres;
+    this.platforms = platforms;
+    this.publishers = publishers;
+    this.timeInterval = timeInterval;
+    this.updateComponents();
   }
 
   /*
@@ -19,31 +19,51 @@ export default class DataManager {
   //
   setGenre(genres) {
     this.genres = genres;
-    this.filteredData = this.data.filter();
+    this.filteredData = this.filter(this.data);
     this.updateComponents();
   }
 
   setPlatform(platforms) {
     this.platforms = platforms;
-    this.filteredData = this.data.filter();
+    this.filteredData = this.filter(this.data);
     this.updateComponents();
   }
 
   setTimeInterval(timeInterval) {
     this.timeInterval = timeInterval;
-    this.filteredData = this.data.filter();
+    this.filteredData = this.filter(this.data);
     this.updateComponents();
   }
 
   setPublisher(publishers) {
     this.publishers = publishers;
-    this.filteredData = this.data.filter();
+    this.filteredData = this.filter(this.data);
     this.updateComponents();
   }
 
   updateComponents() {
-    for(let component of components_to_update) {
-      component.update(this.filteredData);
+    for(let i = 0 ; i < this.components_to_update.length ; ++i) {
+      (this.components_to_update[i]).update(this.filteredData);
     }
+  }
+
+  filter() {
+    this.filteredData = []
+    for (let i = 0 ; i < this.data.length ; ++i) {
+      if (this.genres.includes(this.data[i].Genre) &&
+          this.platforms.includes(this.data[i].Platform) &&
+          this.timeInterval[0] <= this.data[i].Year_of_Release &&
+          this.data[i].Year_of_Release <= this.timeInterval[1] &&
+          this.publishers.includes(this.data[i].Publisher)) {
+            this.filteredData.push(this.data[i])
+          }
+    }
+    return this.filteredData
+  }
+
+  update(newData) {
+    this.data = newData
+    this.filteredData = this.filter(this.data)
+    updateComponents()
   }
 }
