abc9760d65553074b49f97069925e6fe659df875
HugoSalt <hugosalt@protonmail.com>
2017-12-05 21:50:17 +0100

    Loading Screen Implemented

---
+++ modules/LoadingScreen/LoadingScreen.js
@@ -0,0 +1,18 @@
+export default class LoadingScreen {
+
+  constructor() {}
+
+  // percentage = float of progress (eg 0.77)
+  setProgress(percentage) {
+    let bar = document.getElementById("loading_bar");
+    bar.style.width = percentage * 400 + "px";
+    console.log("hello");
+  }
+
+  hide() {
+    let loadingScreen = document.getElementById("loading_screen");
+    loadingScreen.style.transform = "translateY(-100%)";
+    setTimeout(() => loadingScreen.remove(), 5000);
+  }
+
+}
